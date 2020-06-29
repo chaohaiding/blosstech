98a066f9b93f18da82fb39aae363582d813ac91a
Chaohai Ding <chaohaiding@hotmail.co.uk>
2020-06-29 12:15:29 +0100

    update

---
+++ sdk/ble.js
@@ -0,0 +1,7 @@
+/**
+* ble class for blosstech
+* This class is used to contruct the connection between front-end darshboard and ble router.
+*/
+
+
+CC:1B:E0:E1:69:FC
--- sdk/blosstech.js
+++ sdk/blosstech.js
@@ -1,5 +1,6 @@
 /**
-*Connection class for blosstech
+* Connection class for blosstech
+* This class is used to contruct the connection between BLE devices and front-end darshboard.
 */
 
 export default class Connection {
@@ -8,29 +9,58 @@ export default class Connection {
   */
   constructor(url) {
     this.url = url;
-    this.port=7379;
+    this.port = 7379;
+
   }
 
   send(data) {
 
   }
 
+
   /**
-  *getECG//1 tick/second
+  * getECG()
+  * @description: targetECG BLE devies sends back 6 Byte data in 60ms with one notification. collect 15*60ms=900 ms
+  * @param:
+  * @return:
   */
+
+
   getECG() {
     //Using LRANGE to fetch the realtime heart rate data
     const url=this.url+":"+this.port+"/LRANGE/ecg/0/-1";
+
+
+
+
+    fetch(url).then(data=>{
+      return data.json();
+    }).then(res=>{
+      console.log(res);
+    })
+    .catch(error=>{
+      console.log(error);
+    });
+  }
+
+
+
+  /**
+   *getTemp()
+   */
+  getTemp(){
+    //Using LRANGE to fetch the realtime heart rate data
+    const url=this.url+":"+this.port+"/LRANGE/temp/0/-1";
     fetch(url).then(data=>{
       return data.json();
     }).then(res=>{console.log(res);})
     .catch(error=>{console.log(error);});
   }
 
-  /**
-  *
-  */
 
+  /**
+  *delete displayed data
+  */
   trimECGData(){
     //LTRIM
     const url=this.url+":"+this.port+"/LTRIM/ecg/0/-1";
